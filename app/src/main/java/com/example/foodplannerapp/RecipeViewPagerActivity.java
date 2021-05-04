c758b2195781ac66e92f6d105be36619d89cdb04
Emil Lysdahl Fahrenholtz <emillysdahlfahrenholtz@Emils-MacBook-Pro.local>
2021-05-04 20:51:38 +0200

    sorteret liste i activities

--- app/src/main/java/com/example/foodplannerapp/RecipeViewPagerActivity.java
+++ app/src/main/java/com/example/foodplannerapp/RecipeViewPagerActivity.java
@@ -6,15 +6,15 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class RecipeViewPagerActivity extends AppCompatActivity {
 
     ViewPager2 viewPager2;
     static RecipeDB recipesDB;
-    List<RecipeItem> tempList;
+    List<RecipeItem> firstTempList;
     List<RecipeItem> mRecipies;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +23,40 @@ public class RecipeViewPagerActivity extends AppCompatActivity {
         recipesDB = RecipeDB.get(this);
 
         viewPager2 = findViewById(R.id.viewPager2);
-        mRecipies = new ArrayList<>();
-        tempList = new ArrayList<>();
-        tempList = recipesDB.getAll();
+        firstTempList = new ArrayList<>();
+        firstTempList = recipesDB.getAll();
 
-        for (RecipeItem ri : tempList) {
-            String name = ri.getRecipeName();
-            String guide = ri.getRecipeGuide();
-            if (!name.equals("") && !guide.equals("")) {
-                mRecipies.add(ri);
-            }
-        }
+        mRecipies = new ArrayList<>();
+        mRecipies = sortList((ArrayList<RecipeItem>) firstTempList);
 
         viewPager2.setAdapter(new RecipeFragmentStateAdapter(this, mRecipies, viewPager2));
 
+    }
+
+    private ArrayList<RecipeItem> sortList(ArrayList<RecipeItem> firstTempList) {
+        ArrayList<RecipeItem> returnableList = new ArrayList<>();
+        ArrayList<RecipeItem> secondTempList = new ArrayList<>();
+
+        for (RecipeItem ri : firstTempList) {
+            String name = ri.getRecipeName();
+            String guide = ri.getRecipeGuide();
+            if (!name.equals("") && !guide.equals("")) {
+                secondTempList.add(ri);
+            }
+        }
+
+        for (RecipeItem recipeItem : secondTempList) {
+            boolean isFound = false;
+
+            for (RecipeItem r : returnableList) {
+                if (r.getRecipeName().equals(recipeItem.getRecipeName()) || (r.equals(recipeItem))) {
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound) returnableList.add(recipeItem);
+        }
+        return returnableList;
     }
 
 }
--- app/src/main/java/com/example/foodplannerapp/SavedRecipeActivity.java
+++ app/src/main/java/com/example/foodplannerapp/SavedRecipeActivity.java
@@ -30,19 +30,38 @@ public class SavedRecipeActivity extends AppCompatActivity{
     setContentView(R.layout.activity_saved_list);
     mSavedRecipesDB = SavedRecipeDB.get(this);
     viewPager2 = findViewById(R.id.viewPager_saved);
-    mRecipies = new ArrayList<>();
     tempList = new ArrayList<>();
     tempList = mSavedRecipesDB.getSavedRecipeList();
+    mRecipies = new ArrayList<>();
+    mRecipies = sortList((ArrayList<RecipeItem>) tempList);
 
-    for (RecipeItem ri : tempList) {
-        String name = ri.getRecipeName();
-        String guide = ri.getRecipeGuide();
-        if (!name.equals("") && !guide.equals("")) {
-            mRecipies.add(ri);
-        }
+    viewPager2.setAdapter(new SavedFragment(this, mRecipies, viewPager2));
+
     }
 
-        viewPager2.setAdapter(new SavedFragment(this, mRecipies, viewPager2));
+    private ArrayList<RecipeItem> sortList(ArrayList<RecipeItem> firstTempList) {
+        ArrayList<RecipeItem> returnableList = new ArrayList<>();
+        ArrayList<RecipeItem> secondTempList = new ArrayList<>();
 
+        for (RecipeItem ri : firstTempList) {
+            String name = ri.getRecipeName();
+            String guide = ri.getRecipeGuide();
+            if (!name.equals("") && !guide.equals("")) {
+                secondTempList.add(ri);
+            }
+        }
+
+        for (RecipeItem recipeItem : secondTempList) {
+            boolean isFound = false;
+
+            for (RecipeItem r : returnableList) {
+                if (r.getRecipeName().equals(recipeItem.getRecipeName()) || (r.equals(recipeItem))) {
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound) returnableList.add(recipeItem);
+        }
+        return returnableList;
     }
 }
