a0c1349cc3222d13fd109972c766b337791c9d8c
Sidsel Korch <sidselkorch@MacBook-Air-tilhrende-Sidsel.local>
2021-05-04 14:44:21 +0200

    updated savedRecipeActivity

--- app/src/main/java/com/example/foodplannerapp/SavedRecipeActivity.java
+++ app/src/main/java/com/example/foodplannerapp/SavedRecipeActivity.java
@@ -20,22 +20,32 @@ import java.util.UUID;
 public class SavedRecipeActivity extends AppCompatActivity{
     ViewPager2 viewPager2;
     static SavedRecipeDB mSavedRecipesDB;
-    //List<RecipeItem>tempList;
+    List<RecipeItem>tempList;
     List<RecipeItem> mRecipies;
 
 
 @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_list);
-        mSavedRecipesDB = SavedRecipeDB.get(this);
-        viewPager2 = findViewById(R.id.viewPager2);
-        mRecipies = new ArrayList<>();
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_saved_list);
+    mSavedRecipesDB = SavedRecipeDB.get(this);
+    viewPager2 = findViewById(R.id.viewPager2);
+    mRecipies = new ArrayList<>();
+    tempList = new ArrayList<>();
+    tempList = mSavedRecipesDB.getSavedRecipeList();
+
+    for (RecipeItem ri : tempList) {
+        String name = ri.getRecipeName();
+        String guide = ri.getRecipeGuide();
+        if (!name.equals("") && !guide.equals("")) {
+            mRecipies.add(ri);
+        }
+    }
+
 
         mRecipies = mSavedRecipesDB.getSavedRecipeList();
 
-        viewPager2.setAdapter(new SavedFragment(this,mRecipies,viewPager2));
+        viewPager2.setAdapter(new SavedFragment(this, mRecipies, viewPager2));
 
     }
 }
-
